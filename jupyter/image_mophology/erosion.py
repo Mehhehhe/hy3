9848ef4e304613d7cc80c8827248d2b211fbba30
Mehhehhe <pakintada@gmail.com>
2022-03-24 12:53:34 +0700

    แก้ไขเงื่อนไขตอนเทียบ window

--- jupyter/image_mophology/erosion.py
+++ jupyter/image_mophology/erosion.py
@@ -34,25 +34,22 @@ window2 = [
 
 def erosion(window, sample):
     flag = 0
-    result = [
-        [0]*20
-    ]
-    temp = [0];column_count = 0;row_count = 0
-    while row_count < len(sample) and column_count < len(sample[0]):
-        if len(temp) == (len(sample[0])-1):
-            temp.append(0);assert len(temp) == 20,"Len of temp is not enough"
-            row_count += 1;column_count = 0
+    result = [[0]*20]
+    temp = [];column_count = 0;row_count = 0
+    while row_count <= len(sample):
+        if column_count == (len(sample[0])-1):
+            temp.append(0);row_count += 1;column_count = 0
             result.append(temp.copy());temp.clear()
-        elif row_count == len(sample)-1: temp = sample[0];result.append(temp);break
+            if row_count == len(sample)-2: result.append([0]*20);break
+            else:continue
         for i in range(len(window)):
             for j in range(len(window[i])):
-                # print("Flag iter: ",flag)
-                if window[i][j] == 1 and (j+column_count < len(sample[0])) and (i+row_count < len(sample)):
+                if window[i][j] == 1 and (j+column_count <= len(sample[0])-1) and (i+row_count <= len(sample)-1):
                     if sample[i+row_count][j+column_count] == window[i][j]: flag += 1
                     if i == len(window)-1 and flag == 4:flag = 0;temp.append(1);break
                     elif i == len(window)-1 and flag != 4:flag = 0;temp.append(0);break
         column_count += 1
     return result
 
-ers = erosion(window1, sample_data)
+ers = erosion(window1, sample_data);print(len(ers))
 for e in ers: print(e)
